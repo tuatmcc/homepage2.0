86bb7ddbbbd87135115b689824d6b76b34c2005c
OJII3 <kawaisounayuriko@gmail.com>
2022-11-24 13:53:55 +0900

    Add gsap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
   )
 }
 
