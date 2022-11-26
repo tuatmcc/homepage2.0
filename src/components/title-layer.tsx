7c0ba8f06b4cb380faeeb8308ae91a1d7c392770
OJII3 <kawaisounayuriko@gmail.com>
2022-11-26 17:36:26 +0900

    Fix error

--- src/components/title-layer.tsx
+++ src/components/title-layer.tsx
@@ -1,14 +1,31 @@
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useRef, useState } from 'react'
 
 const TitleLayer = (props: { children: ReactElement; className: string }) => {
+  const layerRef = useRef<HTMLDivElement>(null)
+  const [layerWidth, setLayerWidth] = useState(0)
+  const [layerHeight, setLayerHeight] = useState(0)
+
+  const handleResize = () => {
+    if (layerRef.current) {
+      setLayerWidth(layerRef.current.clientWidth)
+      setLayerHeight(layerRef.current.clientHeight)
+    }
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
     <div
       style={{
         position: 'absolute',
         padding: '0 0',
         margin: '0 0',
-        width: '100vw',
-        height: '100vh',
+        width: layerWidth || '100%',
+        height: layerHeight || '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -16,6 +33,7 @@ const TitleLayer = (props: { children: ReactElement; className: string }) => {
         overflow: 'hidden',
       }}
       className={props.className}
+      ref={layerRef}
     >
       {props.children}
     </div>
