5b3d97ba9edd131075625ccbfb37a023936e4b89
OJII3 <kawaisounayuriko@gmail.com>
2022-11-26 15:14:29 +0900

    update title layer

--- src/components/title-layer.tsx
+++ src/components/title-layer.tsx
@@ -1,19 +1,25 @@
-import { ReactElement } from "react"
+import {ReactElement} from "react"
 
-const TitleLayer = (props: {children: ReactElement, className: string}) => {
+const TitleLayer = (props: { children: ReactElement, className: string }) => {
   return (
     <div
       style={{
         position: 'absolute',
+        padding: '0 0',
+        margin: '0 0',
         width: '100vw',
         height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
         overflow: 'hidden',
       }}
       className={props.className}
-      >
+    >
       {props.children}
     </div>
-  )
+  );
 }
 
 export default TitleLayer
