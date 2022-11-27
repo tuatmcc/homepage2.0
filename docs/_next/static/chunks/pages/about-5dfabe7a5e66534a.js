6857c0903a1f1bed4e3a361517842817db521c82
OJII3 <kawaisounayuriko@gmail.com>
2022-11-27 13:17:59 +0900

    Fix configs

--- docs/_next/static/chunks/pages/about-5dfabe7a5e66534a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{9212:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return t(2444)}])},2444:function(n,u,t){"use strict";t.r(u);var _=t(5893);let e=()=>(0,_.jsx)(_.Fragment,{});u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=9212)}),_N_E=n.O()}]);
