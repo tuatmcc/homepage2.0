0b6b8f1d8bb92209c9fa689d4bf14e58180807e8
OJII3 <kawaisounayuriko@gmail.com>
2022-11-27 19:09:35 +0900

    Update top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
 
       <HomeContainer>
         <HomeLayer>
-          <h1 className={styles.title}>
+          <h1 className={styles.title} style={{ zIndex: 20 }}>
             <Image alt='logo' width={130} height={130} src='/mcc-logo.svg' />M
             <span style={{ fontSize: '1rem' }}>icro</span>C
             <span style={{ fontSize: '1rem' }}>omputer</span>C
@@ -29,12 +29,14 @@ const Home = () => {
           </h1>
         </HomeLayer>
         <HomeLayer>
-          <h2 className={styles.subTitle}>東京農工大学公認サークル</h2>
+          <div className={styles.curtain}>
+            <h2 className={styles.subTitle}>東京農工大学公認サークル</h2>
+          </div>
         </HomeLayer>
         <HomeLayer>
           <iframe
             src='Builds/index.html'
-            style={{ height: '100%', width: '100%' }}
+            style={{ height: '100%', width: '100%', zIndex: 1 }}
           />
         </HomeLayer>
       </HomeContainer>
