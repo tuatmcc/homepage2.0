bb5bc7faf27c4512a61406eabd0901f2e42e1857
OJII3 <kawaisounayuriko@gmail.com>
2022-11-30 15:24:24 +0900

    fix
    update

--- .github/scripts/replace.js
+++ .github/scripts/replace.js
@@ -1,11 +1,11 @@
 module.exports = (workspace) => {
   const fs = require('fs')
-  const filePath = fs.readdirSync(`${workspace}/out`).map((filePath) => {
+  const filePaths = fs.readdirSync(`${workspace}/out`).map((filePath) => {
     const absolutePath = `${workspace}/out/${filePath}`
-    const newFile = fs.readFileSync(absolutePath, 'utf8').replace(
-      /href="\/_next/g,
-      'href="/mcc-website/_next'
-    ).replace(/src="/g, 'src="/mcc-website')
+    const newFile = fs
+      .readFileSync(absolutePath, 'utf8')
+      .replace(/href="/g, 'href="/mcc-website')
+      .replace(/src="/g, 'src="/mcc-website')
     fs.writeFileSync(absolutePath, newFile, 'utf8')
   })
 }
