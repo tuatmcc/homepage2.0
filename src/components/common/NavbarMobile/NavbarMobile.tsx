88cc847c27859f285bd9824bdc8f3ec2df987358
OJII3 <84656786+OJII3@users.noreply.github.com>
2022-12-14 21:45:09 +0900

    モバイル版ナビゲーションを整備

---
+++ src/components/common/NavbarMobile/NavbarMobile.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { FC, useState } from 'react';
+
+import CloseIcon from '../Icons/CloseIcon';
+import EnvelopeIcon from '../Icons/EnvelopeIcon';
+import MenuBurgerIcon from '../Icons/MenuBurgerIcon';
+
+import styles from './style.module.css';
+
+import Drawer from '~/components/Drawer/Drawer';
+import BrowserIcon from '~/components/common/Icons/BrowserIcon';
+import HomeIcon from '~/components/common/Icons/HomeIcon';
+import InfoIcon from '~/components/common/Icons/InfoIcon';
+import { ROUTES } from '~/constants/routes';
+
+const NavbarMobile: FC = () => {
+  // Drawerの開閉状態
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  return (
+    <div className={styles.navbarMobile}>
+      <Drawer isOpen={isOpened} />
+      <nav className={styles.navbar}>
+        <ul className={styles.navIn}>
+          <Link href={ROUTES.HOME.PATH} className={styles.link}>
+            <HomeIcon />
+          </Link>
+          <Link href={ROUTES.ABOUT.PATH} className={styles.link}>
+            <InfoIcon />
+          </Link>
+          <Link href={ROUTES.ACTIVITIES.PATH} className={styles.link}>
+            <BrowserIcon />
+          </Link>
+          <Link href="hogehoge" className={styles.link}>
+            <EnvelopeIcon />
+          </Link>
+          <button className={styles.link} onClick={() => setIsOpened(!isOpened)} onBlur={() => setIsOpened(false)}>
+            {isOpened ? <CloseIcon /> : <MenuBurgerIcon />}
+          </button>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavbarMobile;
--- src/components/common/NavbarPC/NavbarPC.tsx
+++ src/components/common/NavbarPC/NavbarPC.tsx
@@ -3,20 +3,14 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { FC } from 'react';
 
-import FullScreenNavigation from '../FullScreenNavigation/FullScreenNavigation';
-import HumbergurIcon from '../HumbergurIcon/HumbergurIcon';
-
 import styles from './style.module.css';
 
 import { BASE_ROUTES_LIST, ROUTES } from '~/constants/routes';
 
-export type HeaderProps = {};
-
-const HeaderTop: FC<HeaderProps> = () => {
+const NavbarPC: FC = () => {
   const pathname = useRouter()
     .pathname.match(/\/.+?(\/|$)/)?.[0]
     .replace(/\/$/, '');
-  console.log(pathname);
   const links = BASE_ROUTES_LIST.map((route) => {
     const isActive = route.PATH == pathname ? true : false;
     return (
@@ -41,4 +35,4 @@ const HeaderTop: FC<HeaderProps> = () => {
   );
 };
 
-export default HeaderTop;
+export default NavbarPC;
