c2f3df6ae85b830108604e844bb83b640e955c49
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-01 21:09:14 +0900

    使わないpackageを削除

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	extends: ['stylelint-config-standard', 'stylelint-config-prettier', 'stylelint-config-recess-order'],
-	plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-declaration-strict-value'],
+	plugins: ['stylelint-order', 'stylelint-declaration-strict-value'],
 	rules: {
 		'string-quotes': 'single',
 		indentation: 2,
