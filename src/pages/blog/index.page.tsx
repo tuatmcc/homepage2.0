ec9a7c3dd525ef305ca9ff993416651a91997b6e
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-01 22:16:02 +0900

    css周辺の修正

--- src/pages/blog/index.page.tsx
+++ src/pages/blog/index.page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
 
 import styles from './style.module.css';
 
@@ -8,6 +9,7 @@ import { Layout } from '~/components/common/Layout';
 import { Tag } from '~/components/common/Tag';
 import { TagList } from '~/components/common/TagList';
 import { PostCollector, PostCollectorProps } from '~/lib/post-collector';
+import { MediaQueryContext } from '~/providers/MediaQueryProvider';
 import { MetaData } from '~/types/meta';
 import classNames from '~/utilities/classNames';
 
@@ -18,54 +20,61 @@ const meta: MetaData = {
 
 // posts will be populated at build time by getStaticProps()
 const WorksPage = ({ posts }: PostCollectorProps) => {
+	const { isMobile } = useContext(MediaQueryContext);
+	posts.sort((a, b) => ((a.frontmatter?.date || 1) < (b.frontmatter?.date || 1) ? 1 : -1));
 	return (
 		<>
 			<Helmet meta={meta} />
 			<Layout>
-				<h1 className={styles.pageTitle}>Blog</h1>
-				<div className={styles.list}>
-					{posts.map((post, index) => {
-						return (
-							<Link
-								href={`blog/${post.slug}`}
-								key={post.slug}
-								className={classNames(styles.listItem, index % 2 === 1 ? styles._reverse : '')}
-							>
-								{post.frontmatter.img ? (
-									<Image
-										className={styles.image}
-										src={post.frontmatter.img}
-										alt={post.frontmatter.title}
-										width={350}
-										height={200}
-										onError={(e) => {
-											e.currentTarget.src = '/mcc-design.jpg';
-										}}
-									/>
-								) : (
-									<Image
-										className={styles.image}
-										src="/mcc-design.jpg"
-										alt={post.frontmatter.title}
-										width={350}
-										height={200}
-									/>
-								)}
-								<div className={styles.text}>
-									<h2 className={styles.title}>{post.frontmatter.title}</h2>
-									{post.frontmatter.tags && (
-										<TagList>
-											{post.frontmatter.tags.map((tag) => (
-												<Tag key={tag}>{tag}</Tag>
-											))}
-										</TagList>
-									)}
-									{post.frontmatter.date && <div>{post.frontmatter.date}</div>}
-									{post.frontmatter.description && <div>{post.frontmatter.description}</div>}
-								</div>
-							</Link>
-						);
-					})}
+				<div className={styles.activities}>
+					<h1 className={styles.pageTitle}>Blog</h1>
+					<div className={styles.list}>
+						{posts.map((post, index) => {
+							return (
+								<Link
+									href={`blog/${post.slug}`}
+									key={post.slug}
+									className={classNames(styles.listItem, !isMobile && index % 2 === 1 ? styles._reverse : '')}
+								>
+									{!isMobile &&
+										(post.frontmatter.img ? (
+											<Image
+												className={styles.image}
+												src={post.frontmatter.img}
+												alt={post.frontmatter.title}
+												width={350}
+												height={200}
+												onError={(e) => {
+													e.currentTarget.src = '/mcc-design.webp';
+												}}
+											/>
+										) : (
+											<Image
+												className={styles.image}
+												src="/mcc-design.webp"
+												alt={post.frontmatter.title}
+												width={350}
+												height={200}
+											/>
+										))}
+									<div className={styles.text}>
+										<h2 className={styles.title}>{post.frontmatter.title}</h2>
+										{post.frontmatter.tags && !isMobile && (
+											<TagList>
+												{post.frontmatter.tags.map((tag) => (
+													<Tag key={tag}>{tag}</Tag>
+												))}
+											</TagList>
+										)}
+										{post.frontmatter.date && <div>{post.frontmatter.date}</div>}
+										{post.frontmatter.description && (
+											<p className={styles.description}>{post.frontmatter.description}</p>
+										)}
+									</div>
+								</Link>
+							);
+						})}
+					</div>
 				</div>
 			</Layout>
 		</>
