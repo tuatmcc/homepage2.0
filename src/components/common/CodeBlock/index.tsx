e5ce9191b8497a52d52b3dd5f3033cbaa2a67101
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-02 09:13:27 +0900

    コピーボタン追加

--- src/components/common/CodeBlock/index.tsx
+++ src/components/common/CodeBlock/index.tsx
@@ -2,17 +2,40 @@ import { FC, ReactNode } from 'react';
 
 import styles from './style.module.css';
 
-export type CodeBlockProps = {
+const copyToClipboard = (text: string) => {
+	navigator.clipboard.writeText(text);
+};
+
+const changeButtonText = (button: HTMLButtonElement) => {
+	button.innerText = 'Copied!';
+	setTimeout(() => {
+		button.innerText = 'Copy';
+	}, 1000);
+};
+
+type CodeBlockProps = {
 	title?: string;
 	children: ReactNode | ReactNode[];
 	className?: string;
 };
 
 export const CodeBlock: FC<CodeBlockProps> = ({ children = '', className = '', title }) => {
+	const id = `codeblock${Math.random().toString()}`;
 	return (
 		<div className={styles.codeBlock}>
 			{title && <span className={styles.title}>{title}</span>}
-			<pre className={`${styles.pre} ${className}`}>{children}</pre>
+			<button
+				className={styles.copyButton}
+				onClick={(event) => {
+					copyToClipboard(`${document.getElementById(id)?.innerText}`);
+					changeButtonText(event.target as HTMLButtonElement);
+				}}
+			>
+				Copy
+			</button>
+			<pre className={`${styles.pre} ${className}`} id={id}>
+				{children}
+			</pre>
 		</div>
 	);
 };
