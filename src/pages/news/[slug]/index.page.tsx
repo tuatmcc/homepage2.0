a04d272b9e77453f6cdec00485a634bb774ff213
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-11 19:57:44 +0900

    remove doubled footer

--- src/pages/news/[slug]/index.page.tsx
+++ src/pages/news/[slug]/index.page.tsx
@@ -3,7 +3,6 @@ import { NextPage } from 'next';
 import { SEO } from '~/features/SEO';
 import { ArticleWrapper } from '~/features/markdown/components/ArticleWrapper';
 import { Post, PostCollector } from '~/features/markdown/post-collector';
-import { Footer } from '~/features/ui/Footer';
 import { Navbar } from '~/features/ui/Navbar';
 import { DynamicRouting } from '~/routes/dynamic-routing';
 
@@ -16,7 +15,6 @@ const NewsPost: NextPage<{ post: Post }> = ({ post }) => {
 			<SEO meta={post.frontmatter} />
 			<Navbar theme='auto' />
 			<ArticleWrapper {...post.frontmatter} contentHtml={post.content} />
-			<Footer />
 		</>
 	);
 };
