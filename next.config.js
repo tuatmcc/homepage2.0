681a1a9a19889b4881ef5c842ff29fab0e608275
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-19 13:25:35 +0900

    npm-scriptsにanalyze-bundleを追加
    add photo
    
    format
    
    restore background image

--- next.config.js
+++ next.config.js
@@ -12,6 +12,6 @@ const nextConfig = {
 module.exports = nextConfig;
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
+	enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({});
