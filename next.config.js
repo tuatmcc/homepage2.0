50edf709978d6aff7685d42061b398f118e141d7
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-20 08:55:16 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ const nextConfig = {
 	trailingSlash: true,
 };
 
-module.exports = nextConfig;
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
 module.exports = withBundleAnalyzer({});
+
+module.exports = nextConfig;
