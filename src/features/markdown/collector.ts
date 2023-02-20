4e700b86e0e641059c2f777a7685d5bc7407f570
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-02-21 04:04:02 +0900

    エラー時にビルドが通らないように修正

--- src/features/markdown/collector.ts
+++ src/features/markdown/collector.ts
@@ -18,53 +18,65 @@ export type Article = {
 	meta: MetaData;
 };
 
-export const Collector = (group: 'blog' | 'news') => {
-	const slugs = readdirSync(`${articlesDirName}/${group}`);
-	const articles: Article[] = slugs.map((slug) => {
-		const { data, content } = matter(readFileSync(`${articlesDirName}/${group}/${slug}/index.md`));
-		const meta = data as MetaData;
-		const markdown = content as string;
+const dummyArticle: Article = {
+	isExist: false,
+	originalBaseURL: '',
+	targetPath: '404',
+	group: 'blog',
+	slug: '',
+	markdown: '',
+	html: '',
+	meta: { title: 'Error' },
+};
 
-		if (meta.img) meta.img = imgsrc(meta.img, group, slug);
+export const Collector = (group: 'blog' | 'news') => {
+	try {
+		const slugs = readdirSync(`${articlesDirName}/${group}`);
+		const articles: Article[] = slugs.map((slug) => {
+			const file = readFileSync(`${articlesDirName}/${group}/${slug}/index.md`);
+			const { data, content } = matter(file);
+			const meta = data as MetaData;
+			const markdown = content as string;
+      // サムネ画像のURLを
+			if (meta.img) meta.img = imgsrc(meta.img, group, slug);
+
+			return {
+				isExist: true,
+				originalBaseURL: `https://raw.githubusercontent.com/tuatmcc/markdown-articles/main/${group}/${slug}/`,
+				targetPath: `${group}/${slug}`,
+				group: group,
+				slug: slug,
+				markdown: markdown,
+				html: '',
+				meta: meta,
+			};
+		});
 
 		return {
-			isExist: true,
-			originalBaseURL: `https://raw.githubusercontent.com/tuatmcc/markdown-articles/main/${group}/${slug}/`,
-			targetPath: `${group}/${slug}`,
-			group: group,
-			slug: slug,
-			markdown: markdown,
-			html: '',
-			meta: meta,
+			getAll() {
+				return articles;
+			},
+			get(slug: string) {
+				const target = articles.find((article, _i) => {
+					return slug === article.slug;
+				});
+				if (target) {
+					return target;
+				} else {
+          console.error('Something went wrong while generating articles')
+					return dummyArticle;
+				}
+			},
 		};
-	});
-
-	return {
-		getAll() {
-			return articles;
-		},
-		get(slug: string) {
-			const target = articles.find((article, _i) => {
-				return slug === article.slug;
-			});
-			if (target) {
-				return target;
-			} else {
-				const errorArticle: Article = {
-					isExist: false,
-					originalBaseURL: '',
-					targetPath: '404',
-					group: group,
-					slug: slug,
-					markdown: '',
-					html: '',
-					meta: {
-						title: 'Error',
-					},
-				};
-
-				return errorArticle;
-			}
-		},
-	};
+	} catch (e) {
+		console.error(e);
+		return {
+			getall() {
+				return [dummyArticle];
+			},
+			get(_slug: string) {
+				return dummyArticle;
+			},
+		};
+	}
 };
