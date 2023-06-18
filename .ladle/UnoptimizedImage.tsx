1bc2f1cc78e0e564cd6f3baae68315a6ae1645bf
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-06-18 11:32:05 +0900

    [update]: vscode settings

--- .ladle/UnoptimizedImage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ComponentPropsWithoutRef, FC } from 'react';
-
-const UnoptimizedImage: FC<ComponentPropsWithoutRef<'img'> & { alt: string; src: string }> = ({
-	src,
-	alt,
-	...props
-}) => <img src={src} alt={alt} {...props} />;
-
-export default UnoptimizedImage;
